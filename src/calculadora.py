550acc19371a63ad4f642a2ea107226063db3b08
vdonato <joao.ddonato@gmail.com>
2024-12-18 22:53:21 -0300

    Escolher Funções

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,7 +1,14 @@
 import sys
 
-def resultado(primeiro, segunda, terceiro):
+def soma(primeiro, segunda, terceiro):
     print(primeiro + segunda + terceiro)
-    
 
-resultado(int(sys.argv[1]), int(sys.argv[2]),int(sys.argv[3]))
+def mult(pri, sec, terc):
+    print(pri * sec * terc)
+    
+if (sys.argv[1] == "+"):
+    soma(int(sys.argv[2]), int(sys.argv[3]),int(sys.argv[4]))
+    
+elif (sys.argv[1] == "x"):
+    mult(int(sys.argv[2]), int(sys.argv[3]),int(sys.argv[4]))   
+.
